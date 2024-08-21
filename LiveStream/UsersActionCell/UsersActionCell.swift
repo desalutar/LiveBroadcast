e7ac55991549479bd510acefc32654d3a834b82d
desalutar <desalutare@gmail.com>
2024-08-21 19:03:45 +0300

    Add user interaction views: Likes, Followers, Following

---
+++ LiveStream/UsersActionCell/UsersActionCell.swift
@@ -0,0 +1,18 @@
+//
+//  UsersActionCell.swift
+//  LiveStream
+//
+//  Created by Ишхан Багратуни on 21.08.24.
+//
+
+import SwiftUI
+
+struct UsersActionCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsersActionCell()
+}
